a79b3aeb91402a870c5f0c74624cb38d0e9b284f
Albert Jialeng Guo <sub.jl@icloud.com>
2022-04-11 17:44:16 +0100

    Update fibonacci_number.py

--- 1_fibonacci_series/fibonacci_number.py
+++ 1_fibonacci_series/fibonacci_number.py
@@ -1,34 +1,15 @@
 import matplotlib.pyplot as plt
 
-def fibonacci(index):
-	if index >= 3:
-		index_list = [1,2]
-		fibo_list = [1,1]
-		fibo1 = 1
-		fibo2 = 1
-		i = 3
-		while i <= index:
-			fib_next = fibo1 + fibo2
-			index_list.append(i)
-			fibo_list.append(fib_next)
-			i += 1
-			fibo1 = fibo2
-			fibo2 = fib_next
-		# return(index_list, fibo_list)
-		fig = plt.figure(figsize=(15,7))
-		plt.plot(index_list, fibo_list)
-		plt.show()
-	else:
-		print("at least first three of fibonacci number")
-		
-fibonacci(3)
-# fibonacci(10)
-# fibonacci(20)
-# fibonacci(50)
-# fibonacci(70)
-# fibonacci(100)
-# fibonacci(150)
-# fibonacci(200)
-# fibonacci(500)
-# fibonacci(1000)
-# fibonacci(1300)
+def fibonacci(Nth_fibonacci_number):
+    x_axis = [i for i in range(1,Nth_fibonacci_number+1)]
+    fibonacci_list = [1,1]
+    for i in range(2, Nth_fibonacci_number): # min of N is 3 <=> min of index is 2
+        new_fibonacci_number = fibonacci_list[i-1] + fibonacci_list[i-2]
+        fibonacci_list.append(new_fibonacci_number)
+    return(fibonacci_list, x_axis)
+
+
+return_list, x_axis = fibonacci(5) # Nth_fibonacci_number>=2
+print(return_list)
+plt.plot(x_axis,return_list)
+plt.show()
